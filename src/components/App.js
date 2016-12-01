7f4da124d2b7a151146fddcb70bd7cb3911a7c45
Keir Staple <keirstaple@comcast.net>
2016-12-01 15:33:59 -0800

    remove bootstrap code

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import base from '../base';
+// import BadgeList from './BadgeList';
 
 class App extends Component {
   constructor() {
@@ -9,20 +10,25 @@ class App extends Component {
     };
   }
 
-  componentDidMount() {
-    base.fetch('badges', {
-      context:this,
-      state: 'badges',
-      asArray: true,
-      then(data) {
-        this.setState({badges:data});
-        console.log(data);
-        console.log(this.state);
-      }
-    });
+  componentWillMount(){
+    // base.fetch('badges', {
+    //   context:this,
+    //   state: 'badges',
+    //   asArray: true
+    // }).then(data => {
+    //   console.log(data);
+    //   this.setState({
+    //     badges: data
+    //   });
+    // }).catch(error => {
+    //   //handle error
+    // });
+    this.ref = base.syncState(`badges`, {context: this, state: "badges", asArray: true});
+
   }
 
   render() {
+    // console.log(this.state.badges);
     return (
       <div className="App">
         <h1>Hello</h1>
---
+++ src/components/Badge.js
@@ -0,0 +1,11 @@
+// import React, {Component} from 'react';
+//
+// class Badge extends Component{
+//   render() {
+//     return(
+//       <h1>Hello from Badge</h1>
+//     );
+//   }
+// }
+//
+// export default Badge;
