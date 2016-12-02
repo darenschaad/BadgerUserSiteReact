b317f2d97400f7a676dc0bda93181262e260501c
Keir Staple <keirstaple@comcast.net>
2016-12-02 12:29:58 -0800

    add some semi-working router stuff

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return(
+      <p>Page Not Found</p>
+    )
+  }
+}
+
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Router, Route, Link, browserHistory } from 'react-router'
 import App from './components/App';
+import BadgeList from './components/BadgeList';
+import Badge from './components/Badge';
+import NotFound from './components/NotFound';
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="/badges" component={BadgeList}>
+        <Route path="/badges/:pushId" component={Badge}/>
+      </Route>
+      <Route path="*" component={NotFound}/>
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
