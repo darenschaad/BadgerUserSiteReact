47fdaeb8847353b896a98f1f4ed4532495842a6e
Keir Staple <keirstaple@comcast.net>
2016-12-20 16:07:45 -0800

    add second random badge and prevent random badges from being the same

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -6,7 +6,7 @@ class Badge extends Component{
     const ourBadge = JSON.parse(localStorageRef);
     const categories = [0,100,200,300,400,500,600,700,800,900];
     const index = categories.indexOf(ourBadge.category);
-    const categoryNames = ["000 - GENERAL KNOWLEDGE", "100 - PHILOSOPHY & PSYCHOLOGY", "200 - RELIGION", "300 - SOCIAL SCIENCE", "400 - LANGUAGES", "500 - SCIENCE", "600 - TECHNOLOGY", "700 - ARTS & RECREATION", "800 - LITERATURE", "900 - HISTORY & GEOGRAPHY"]
+    const categoryNames = ["000 - GENERAL KNOWLEDGE", "100 - PHILOSOPHY & PSYCHOLOGY", "200 - RELIGION", "300 - SOCIAL SCIENCE", "400 - LANGUAGES", "500 - SCIENCE", "600 - TECHNOLOGY", "700 - ARTS & RECREATION", "800 - LITERATURE", "900 - HISTORY & GEOGRAPHY"];
     const textColors = ["#4C4C4C", "#0079A5", "#66008D", "#4D782D", "#C97100", "#25895A", "#000073", "#988967", "#76193C", "#985721"];
     const backgroundColors = ["#989DA7", "#DCF0FF", "#D0C0D6", "#CEDFB0", "#EEC99A", "#9EBAAC", "#B5B5CA", "#FDE192", "#DBC2CC", "#D8C2A9"];
     const category = categoryNames[index];
--- src/components/RandomBadge.js
+++ src/components/RandomBadge.js
@@ -8,13 +8,38 @@ class RandomBadge extends Component {
     function getRandomNumber(length) {
       return Math.floor(Math.random() * (length));
     }
-    let badge1 = badgeArray[getRandomNumber(length)];
-    console.log(badge1);
+    let rand1 = getRandomNumber(length);
+    let rand2 = getRandomNumber(length);
+    while (rand1 === rand2) {
+      rand2 = getRandomNumber(length);
+    }
+
+    let badge1 = badgeArray[rand1];
+    let badge2 = badgeArray[rand2];
+
+    const categories = [0,100,200,300,400,500,600,700,800,900];
+
+    let index1 = categories.indexOf(badge1.category);
+
+    const categoryNames = ["000 - GENERAL KNOWLEDGE", "100 - PHILOSOPHY & PSYCHOLOGY", "200 - RELIGION", "300 - SOCIAL SCIENCE", "400 - LANGUAGES", "500 - SCIENCE", "600 - TECHNOLOGY", "700 - ARTS & RECREATION", "800 - LITERATURE", "900 - HISTORY & GEOGRAPHY"];
+    const textColors = ["#4C4C4C", "#0079A5", "#66008D", "#4D782D", "#C97100", "#25895A", "#000073", "#988967", "#76193C", "#985721"];
+    const backgroundColors = ["#989DA7", "#DCF0FF", "#D0C0D6", "#CEDFB0", "#EEC99A", "#9EBAAC", "#B5B5CA", "#FDE192", "#DBC2CC", "#D8C2A9"];
+
     return (
       <div>
+        <h2>RANDOM BADGES</h2>
+        <hr></hr>
         <h1>Activity: {badge1.name}</h1>
-        <img src={badge1.imageUrl}></img>
-        <h1></h1>
+        <img className='detail-image' src={badge1.imageUrl} alt={badge1.names}></img>
+        <h3>To do: {badge1.description} <br></br> {badge1.comments}</h3>
+        <hr></hr>
+        <h3>Proof: {badge1.proof}</h3>
+
+          <h1>Activity: {badge2.name}</h1>
+          <img className='detail-image' src={badge2.imageUrl} alt={badge2.names}></img>
+          <h3>To do: {badge2.description} <br></br> {badge2.comments}</h3>
+          <hr></hr>
+          <h3>Proof: {badge2.proof}</h3>
       </div>
     );
   }
