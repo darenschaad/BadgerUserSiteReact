fdf2f81e6f23d0376d96aab1457e9e5cc868bcaf
Keir Staple <keirstaple@comcast.net>
2017-01-28 13:55:41 -0800

    make stateless BadgeTile component

---
+++ src/components/BadgeTile.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import * as branding from './branding';
+
+const BadgeTile = (props) => {
+  let index = branding.categories.indexOf(props.badge.category);
+
+  let category = branding.categoryNames[index];
+
+  let textColor = branding.textColors[index];
+
+  let backgroundColor = branding.backgroundColors[index];
+
+  return(
+    <div key={props.idx} className="standard-search-individual-names">
+      <div style={{backgroundColor: backgroundColor}} className='badge-tile hover-hand random-badge-content' onClick={() => props.goToBadge(props.badge, props.searchValue, props.searchState)}>
+        <div className="badge-tile-image-details">
+          <img className='detail-image' src={props.badge.imageUrl} alt={props.badge.names}></img>
+          <div className="badge-tile-details">
+            <h1 style={{color: textColor }} >{props.badge.name}</h1>
+            <span style={{color: textColor }} className="badge-tile-subtitle">To do: </span>
+            <span className="badge-tile-description">{props.badge.description}</span>
+            <br />
+            <span style={{color: textColor }} className="badge-tile-subtitle">Proof: </span>
+            <span className="badge-tile-description">{props.badge.proof}</span>
+          </div>
+        </div>
+        <h1 style={{color: textColor }} className="badge-tile-category">{category}</h1>
+      </div>
+    </div>
+  )
+}
+
+export default BadgeTile;
