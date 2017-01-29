33ec691e8de504bf6c001bd199b6fd858e5721a1
Daren Schaad <darenschaad@gmail.com>
2017-01-29 13:07:56 -0800

    fixed warning on categories component

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -11,7 +11,7 @@ class Categories extends Component {
   goToCategory(currentCategory) {
     // console.log(value);
     localStorage.setItem(`category`, JSON.stringify(currentCategory));
-    if (currentCategory == 0) {
+    if (currentCategory === 0) {
       this.context.router.transitionTo(`/categories/${currentCategory}00`);
     } else{
       this.context.router.transitionTo(`/categories/${currentCategory}`);
