e3f3ec265ba712ef411ab07980dc57f8d3c6ac41
Daren Schaad <darenschaad@gmail.com>
2017-10-13 00:26:53 -0700

    filtered out badges by a particular challenge set to display in the challenge list component

--- src/components/ChallengeList.js
+++ src/components/ChallengeList.js
@@ -38,8 +38,9 @@ class ChallengeList extends Component {
       );
     }else {
       let url = window.location.href;
-      var urlSplit = (url.split("challenges/"));
-      var currentChallenge = urlSplit[1];
+      let urlSplit = (url.split("challenges/"));
+      let currentChallenge = urlSplit[1];
+            
       try {
 
       } catch (e) {
@@ -50,14 +51,47 @@ class ChallengeList extends Component {
       const challenges = ['good-sport', 'kitchen-sciences', 'know-it-all', 'master-gardener', 'media-consumption', 'media-production', 'model-citizen', 'party-animal', 'professional-development', 'survival-skills', 'textiles'];
 
       const index = challenges.indexOf(currentChallenge);
-      console.log(index);
 
+      const divIndex = "challenge-list-index-" + currentChallenge;
 
+      const textColors = ["#4C4C4C", "#0079A5", "#66008D", "#4D782D", "#C97100", "#25895A", "#000073", "#988967", "#76193C", "#985721", "#4C4C4C"];
 
+      const textColor = textColors[index];
 
+      let challengeBadgeCount = 0;
+
+      let filteredByChallenge = this.props.badges.map(
+        (badge, idx) => {
+          if(badge.challenges.toLowerCase().replace(" ","-").includes(currentChallenge)){
+            challengeBadgeCount++;
+            return (
+              <div className={divIndex}  key={idx} >
+                <div className="category-list-div" onClick={() => this.goToBadge(badge)}>
+                  <img className='category-list-image' src={badge.imageUrl} alt={badge.name}></img>
+                  <h4 className="category-list-text" style={{color: textColor}}>{badge.name}</h4>
+                </div>
+              </div>
+            );
+          }
+          return filteredByChallenge;
+        }
+      );
       return(
         <div>
-          <p> Hello</p>
+          <select value={currentChallenge} onChange={this.handleChange} className="category-list-category-title">
+            <option value="000">000 - GENERAL KNOWLEDGE</option>
+            <option value="100">100 - PHILOSOPHY & PSYCHOLOGY</option>
+            <option value="200">200 - RELIGION</option>
+            <option value="300">300 - SOCIAL SCIENCE</option>
+            <option value="400">400 - LANGUAGES</option>
+            <option value="500">500 - SCIENCE</option>
+            <option value="600">600 - TECHNOLOGY</option>
+            <option value="700">700 - ARTS & RECREATION</option>
+            <option value="800">800 - LITERATURE</option>
+            <option value="900">900 - HISTORY & GEOGRAPHY</option>
+          </select>
+          ({challengeBadgeCount})
+          {filteredByChallenge}
         </div>
       )
 
